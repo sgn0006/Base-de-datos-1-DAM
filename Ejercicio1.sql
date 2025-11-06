4578a1844f013c3d906f8939851150d70dec5618
sgn0006 <sgn0006@alu.medac.es>
2025-11-06 11:33:49 +0100

    06/11/2025

---
+++ Ejercicio1.sql
@@ -0,0 +1,23 @@
+drop database if exists clientes;
+create database clientes;
+use clientes;
+
+create table clientes(
+	DNI int primary key auto_increment,
+		apellidos varchar(50) not null,
+		nombre varchar(50) not null,
+		direccion varchar(100),
+		fecha_nac int not null
+);
+
+create table producto(
+		codigo int primary key auto_increment,
+	precio_unit int not null,
+    nombre varchar(50) not null
+);
+
+create table proveedor(
+		NIF int primary key auto_increment,
+	direccion varchar(50) not null,
+    nombre varchar(50) not null
+);
